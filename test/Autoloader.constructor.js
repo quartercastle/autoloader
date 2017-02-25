5e251953c1ee7690eb456453982f9ce935be87c2
Frederik Kvartborg <fka@skemax.dk>
2017-02-25 12:44:56 +0100

    Added tests

--- test/Autoloader.constructor.js
+++ test/Autoloader.constructor.js
@@ -4,26 +4,55 @@ const { expect } = require('chai')
 const Autoloader = require('../lib/Autoloader')
 
 describe('Autoloader.constructor', () => {
-  it('Should create a new autoloader instance with default options', () => {
-    const { modules } = new Autoloader(path.resolve(__dirname, 'modules'))
+  it('Should create a new autoloader instance', () => {
+    const autoloader = new Autoloader(path.resolve(__dirname, './modules'))
+    expect(autoloader).to.be.instanceOf(Autoloader)
+  })
 
+  it('Should include the correct folders and create a namespace module', () => {
+    const { modules } = new Autoloader(path.resolve(__dirname, './modules'))
     expect(modules).to.be.deep.equal({
-      file: {
-        key: 'value'
+      'other-modules': {
+        test: ['other module']
       },
       submodules: {
-        file: true
-      }
+        data: {
+          data: {
+            samples: ['test', 'hello']
+          }
+        },
+        module: true
+      },
+      file: { key: 'value' }
     })
   })
 
-  it('Should throw an exception if no path is specified', () => {
-    expect(() => new Autoloader()).to.throw('Path should be a string')
+  it('Should ignore the folder submodules and file.js', () => {
+    const { modules } = new Autoloader(path.resolve(__dirname, './modules'), {
+      ignore: ['submodules', 'file.js']
+    })
+    expect(modules.submodules).to.be.undefined
+    expect(modules.file).to.be.undefined
   })
 
-  it(`Should throw an exception if the specified path doesn't exist`, () => {
-    expect(() => new Autoloader('not/working')).to.throw(
-      'Couldn\'t find the path not/working'
+  it('Should only include json files', () => {
+    const { modules } = new Autoloader(path.resolve(__dirname, './modules'), {
+      include: ['json']
+    })
+
+    expect(modules.submodules.data.data).to.not.be.undefined
+    expect(modules.file).to.be.undefined
+  })
+
+  it(`Should throw an exception if the path doesn't exist`, () => {
+    expect(() => new Autoloader('./does-not-exist')).to.throw(
+      `Couldn't find the path ./does-not-exist`
+    )
+  })
+
+  it(`Should throw an exection if the first argument isn't a string`, () => {
+    expect(() => new Autoloader({})).to.throw(
+      'First argument should be a path'
     )
   })
 })
